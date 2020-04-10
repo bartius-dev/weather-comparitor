c77d8f0fc32452da43eba3c5e399aea7710d8b3e
bartius-nigel <nigel.cox@gmail.com>
2020-04-09 22:50:53 -0600

    added an input component

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,10 @@
 <script>
-  export let name;
+  import TextInputBox from './TextInputBox.svelte';
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
+  <h1>Welcome!</h1>
+  <TextInputBox let:city={cityName}/>
 </main>
 
 <style>
@@ -19,8 +15,7 @@
     margin: 0 auto;
   }
   h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
+    color: lawngreen;
     font-size: 4em;
     font-weight: 100;
   }
