ad8374d7f365844c92254d7fbfbd194c630ee19a
shauntober <aushn4@gmail.com>
2020-10-01 15:09:50 +0100

    prettify

--- src/stores/Cities.js
+++ src/stores/Cities.js
@@ -5,11 +5,12 @@ function createCitiesStore() {
 
   return {
     subscribe,
-    addCity: (city) => update((cities) => {
-      const exists = cities.find(c => c.city === city.city);
-      if (!exists) return [...cities, city];
-      return cities;
-    }),
+    addCity: (city) =>
+      update((cities) => {
+        const exists = cities.find((c) => c.city === city.city);
+        if (!exists) return [...cities, city];
+        return cities;
+      }),
     deleteCity: (cityToRemove) =>
       update((cities) =>
         cities.filter((city) => {
