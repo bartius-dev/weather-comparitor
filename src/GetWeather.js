5b50b50eb33cc26e37c4ad40e533bbfdef7a2668
shauntober <aushn4@gmail.com>
2020-10-04 14:09:27 +0100

    oops

--- src/GetWeather.js
+++ src/GetWeather.js
@@ -1,6 +1,6 @@
 export default async function getWeather(newCityName, newCountryName) {
   const apiKey =
-    process.env.WEATHER_API_KEY || "8f02459098604a039aac044d57d0a727";
+    process.env.WEATHER_API_KEY || "WEATHER_API_KEY";
   const response = await fetch(
     `https://api.weatherbit.io/v2.0/forecast/daily?city=${newCityName}&country=${newCountryName}&key=${apiKey}`
   );
